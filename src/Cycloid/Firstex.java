9d5c24307e02f0a26197300360432a7ba30c75fe
Geonmo <Geonmo@210.219.52.18>
2015-02-25 17:12:13 +0900

    Init for Cycloid

--- src/Cycloid/Firstex.java
+++ src/Cycloid/Firstex.java
@@ -8,8 +8,6 @@ package Cycloid;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.ArrayList;
-import java.util.List;
 import org.jfree.chart.JFreeChart;
 
 /**
